3c5fbdbab6afa901f6597b118e56a307b355f8e9
Xavier Alvarez <95555833+SoulsTogetherX@users.noreply.github.com>
2023-11-27 15:29:45 -0800

    Towers (#2)
    * GridObjects
    
    * basic grid snapping
    
    * changed positioning system in Grid
    
    * Grid additions and Tower starting
    
    * adding tower types

--- src/objects/Towers/Tower.gd
+++ src/objects/Towers/Tower.gd
@@ -1,11 +1,8 @@
 @tool
 class_name Tower extends GridObject
 
-enum TowerType {EMPTY = 0, ArrowKnight = 1, ShieldWizard = 2}
+@onready var texture = $main.texture;
 
 func _ready() -> void:
 	super();
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	adjust_size(texture);
